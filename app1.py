b27e186dc3fcb9b3700667c84c0c8cf9ba99af03
Praveen Toppo <impraveen.n91@gmail.com>
2022-02-06 23:20:38 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+from PIL import Image
+
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+def welcome():
+    return "Welcome All"
+
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Bank Authenticator")
+    variance = st.text_input("Variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
+
+if __name__=='__main__':
+    main()
